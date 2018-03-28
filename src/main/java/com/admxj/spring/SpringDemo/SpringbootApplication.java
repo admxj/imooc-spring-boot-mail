8fbca86014a43762872af2b955d5747ef8fd167d
admxj <admxj@126.com>
2018-03-29 07:38:08 +0800

    templates

--- src/main/java/com/admxj/spring/SpringDemo/SpringbootApplication.java
+++ src/main/java/com/admxj/spring/SpringDemo/SpringbootApplication.java
@@ -1,14 +1,12 @@
 package com.admxj.spring.SpringDemo;
 
-import com.admxj.spring.SpringDemo.properties.JdbcProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(JdbcProperties.class)
-public class SpringbootApplication {
-
+public class SpringbootApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
