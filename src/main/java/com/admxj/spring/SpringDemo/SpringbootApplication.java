668f3b78a285fba72d74e580cee59f1c6561cc0d
admxj <admxj@126.com>
2018-03-30 19:43:22 +0800

    定时任务

--- src/main/java/com/admxj/spring/SpringDemo/SpringbootApplication.java
+++ src/main/java/com/admxj/spring/SpringDemo/SpringbootApplication.java
@@ -4,8 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+
+//开启定时任务
+@EnableScheduling
+
+//开启异步执行
+@EnableAsync
 public class SpringbootApplication{
 
     public static void main(String[] args) {
