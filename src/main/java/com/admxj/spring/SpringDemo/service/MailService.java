e5ac47b42f1f57e43ac3b089c93a8c9488734861
项金 <admxj@126.com>
2018-10-06 21:24:38 +0800

    JavaMailSender

--- src/main/java/com/admxj/spring/SpringDemo/service/MailService.java
+++ src/main/java/com/admxj/spring/SpringDemo/service/MailService.java
@@ -1,7 +1,32 @@
 package com.admxj.spring.SpringDemo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by admxj on 2018/10/6.
  */
+@Service
 public class MailService {
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleMail(String to, String subject, String contnet){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(contnet);
+        message.setFrom(from);
+
+        mailSender.send(message);
+    }
+
 }
---
+++ src/test/java/com/admxj/spring/SpringDemo/service/MailServiceTest.java
@@ -0,0 +1,25 @@
+package com.admxj.spring.SpringDemo.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admxj on 2018/10/6.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailServiceTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Test
+    public void sendSimpleMail() {
+        mailService.sendSimpleMail("admxj@126.com","测试spring boot mail","测试spring boot mial 内容");
+    }
+}
