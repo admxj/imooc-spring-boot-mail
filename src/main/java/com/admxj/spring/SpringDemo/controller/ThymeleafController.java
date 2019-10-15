b3244cca9a859438bfa7a0fed1778cafe5288fdc
jinxiang <jin.xiang@ly.com>
2019-10-15 10:15:03 +0800

    update

--- src/main/java/com/admxj/spring/SpringDemo/controller/ThymeleafController.java
+++ src/main/java/com/admxj/spring/SpringDemo/controller/ThymeleafController.java
@@ -2,9 +2,14 @@ package com.admxj.spring.SpringDemo.controller;
 
 import com.admxj.spring.SpringDemo.entity.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -14,9 +19,13 @@ public class ThymeleafController {
     private Resource resource;
 
     @RequestMapping(value = "/index")
-    public String index(ModelMap map){
+    public Model index(ModelMap map, HttpServletRequest request) {
+        ModelMap modelMap;
+        ModelAndView modelAndView;
+
         map.addAttribute("resource", resource);
 
-        return "thymeleaf/index";
+//        return "thymeleaf/index";
+        return null;
     }
 }
