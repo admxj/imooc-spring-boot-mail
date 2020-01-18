5213526e6c458d6f85e35b3d2525a8c88e1ad584
admxj <admxj@126.com>
2020-01-19 00:43:04 +0800

    OneThread

--- src/main/java/com/admxj/spring/boot/threadpool/EveryTaskOneThread.java
+++ src/main/java/com/admxj/spring/boot/threadpool/EveryTaskOneThread.java
@@ -4,4 +4,20 @@ package com.admxj.spring.boot.threadpool;
  * @author admxj
  */
 public class EveryTaskOneThread {
+
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Task());
+        thread.start();
+    }
+
+
+    static class Task implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("启动一个线程: " + Thread.currentThread().getId());
+        }
+    }
+
 }
