f6111a9435e96854c71fbedbf0bf46128edef134
admxj <admxj@126.com>
2020-01-21 02:48:04 +0800

    es

---
+++ src/main/java/com/admxj/spring/boot/nginx/ElasticSearchUtils.java
@@ -0,0 +1,11 @@
+package com.admxj.spring.boot.nginx;
+
+/**
+ * @author admxj
+ */
+public class ElasticSearchUtils {
+
+    public void send(){
+
+    }
+}
--- src/main/java/com/admxj/spring/boot/utils/JsonUtils.java
+++ src/main/java/com/admxj/spring/boot/utils/JsonUtils.java
@@ -4,30 +4,39 @@ package com.admxj.spring.boot.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.util.List;
 
 /**
- *
+ * @author leechenxiang
+ * @version V1.0
  * @Title: JsonUtils.java
  * @Package com.lee.utils
  * @Description: 自定义响应结构, 转换类
  * Copyright: Copyright (c) 2016
  * Company:Nathan.Lee.Salvatore
- *
- * @author leechenxiang
  * @date 2016年4月29日 下午11:05:03
- * @version V1.0
  */
 public class JsonUtils {
 
-    // 定义jackson对象
+    /**
+     * 定义jackson对象
+     */
     private static final ObjectMapper MAPPER = new ObjectMapper();
+
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+    }
 
     /**
      * 将对象转换成json字符串。
      * <p>Title: pojoToJson</p>
      * <p>Description: </p>
+     *
      * @param data
      * @return
      */
@@ -45,7 +54,7 @@ public class JsonUtils {
      * 将json结果集转化为对象
      *
      * @param jsonData json数据
-     * @param clazz 对象中的object类型
+     * @param beanType    对象中的object类型
      * @return
      */
     public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
@@ -62,11 +71,12 @@ public class JsonUtils {
      * 将json数据转换成pojo对象list
      * <p>Title: jsonToList</p>
      * <p>Description: </p>
+     *
      * @param jsonData
      * @param beanType
      * @return
      */
-    public static <T>List<T> jsonToList(String jsonData, Class<T> beanType) {
+    public static <T> List<T> jsonToList(String jsonData, Class<T> beanType) {
         JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
         try {
             List<T> list = MAPPER.readValue(jsonData, javaType);
